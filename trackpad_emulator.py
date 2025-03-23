2b753d6b95a02f4d14f9f22f36c25072f689c792
Jade <hatesjade@gmail.com>
2025-03-23 19:20:29 +0800

    class TouchscreenHandler done; remove useless snippet

--- trackpad_emulator.py
+++ trackpad_emulator.py
@@ -7,33 +7,6 @@ import argparse
 import sys
 
 
-def find_touchscreen_udev():
-    context = pyudev.Context()
-    for device in context.list_devices(subsystem="input"):
-        if (
-            device.properties.get("ID_INPUT_TOUCHSCREEN") == "1"
-            and device.device_node is not None
-        ):
-            dev = InputDevice(device.device_node)
-            return dev
-    print("Touch device not found")
-    sys.exit()
-
-
-def apply_rotation(x, y, rotation, max_x, max_y):
-    """Transform coordinates based on rotation"""
-    if rotation == 0:
-        return x, y
-    elif rotation == 180:
-        return (max_x - x, max_y - y)
-    elif rotation == 90:
-        return (y, max_x - x)
-    elif rotation == 270:
-        return (max_y - y, x)
-    else:
-        raise ValueError("Invalid rotation value")
-
-
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -77,66 +50,10 @@ def parse_arguments():
     return parser.parse_args()
 
 
-def toggle_original_input(touch_dev_name, enabled):
-    if enabled:
-        subprocess.run(
-            ["hyprctl", "-r", "keyword", f"device[{touch_dev_name}]:enabled", "1"]
-        )
-        print(f"{touch_dev_name} enabled")
-    else:
-        subprocess.run(
-            ["hyprctl", "-r", "keyword", f"device[{touch_dev_name}]:enabled", "0"]
-        )
-        print(f"{touch_dev_name} disabled")
-
-
-def get_device_xy_limit(touch_dev):
-    max_x = max_y = 0
-    for abs_code, abs_info in touch_dev.capabilities().get(ecodes.EV_ABS, []):
-        if abs_code == ecodes.ABS_X:
-            max_x = abs_info.max
-        elif abs_code == ecodes.ABS_Y:
-            max_y = abs_info.max
-    return max_x, max_y
-
-
-def capture_report(touch_dev_name, max_x, max_y, rotation):
-    print(f"Capturing input from {touch_dev_name}")
-    print(f"Max X: {max_x}, Max Y: {max_y}")
-    print(f"Active rotation: {rotation}°")
-
-
-def qualify(x):
-    qualified_x = 0
-    if x > 0:
-        qualified_x = 1
-    if x < 0:
-        qualified_x = -1
-    return qualified_x
-
-
-args = parse_arguments()
-ORIENTATION = args.rotate
-TAP_MOVEMENT_THRESHOLD = args.tap_threshold
-TAP_DURATION_THRESHOLD = args.tap_duration
-SCROLL_MOVEMENT_THRESHOLD = args.scroll_threshold
-SCROLL_SENSITIVITY = args.scroll_sensitivity
-CURSOR_SENSITIVITY = args.cursor_sensitivity
-
-device = uinput.Device(
-    [
-        uinput.REL_X,
-        uinput.REL_Y,
-        uinput.BTN_LEFT,
-        uinput.BTN_RIGHT,
-        uinput.REL_WHEEL,
-        uinput.REL_HWHEEL,
-    ]
-)
-
-
 class TrackPad:
     def __init__(self) -> None:
+        self.scroll_sensitivity = args.scroll_sensitivity
+        self.cursor_sensitivity = args.cursor_sensitivity
         self.device = uinput.Device(
             [
                 uinput.REL_X,
@@ -148,15 +65,23 @@ class TrackPad:
             ]
         )
 
+    def _qualify(self, x):
+        qualified_x = 0
+        if x > 0:
+            qualified_x = 1
+        if x < 0:
+            qualified_x = -1
+        return qualified_x
+
     def move_cursor(self, current_x, current_y, last_x, last_y):
-        rel_x = int((current_x - last_x) * CURSOR_SENSITIVITY)
-        rel_y = int((current_y - last_y) * CURSOR_SENSITIVITY)
+        rel_x = int((current_x - last_x) * self.cursor_sensitivity)
+        rel_y = int((current_y - last_y) * self.cursor_sensitivity)
         self.device.emit(uinput.REL_X, rel_x)
         self.device.emit(uinput.REL_Y, rel_y)
 
     def scroll_wheel(self, current_x, current_y, last_x, last_y):
-        rel_x = qualify(current_x - last_x) * SCROLL_SENSITIVITY
-        rel_y = qualify(current_y - last_y) * SCROLL_SENSITIVITY
+        rel_x = self._qualify(current_x - last_x) * self.scroll_sensitivity
+        rel_y = self._qualify(current_y - last_y) * self.scroll_sensitivity
         self.device.emit(uinput.REL_HWHEEL, -rel_x)
         self.device.emit(uinput.REL_WHEEL, rel_y)
 
@@ -181,26 +106,97 @@ class TrackPad:
                 self.device.emit(uinput.BTN_RIGHT, 0)
 
 
+class NoValidDeviceError(Exception):
+    """Exception raised when no valid touchscreen device is found."""
+
+    def __init__(self, message="No touchscreen detected"):
+        super().__init__(message)
+
+
+class TouchscreenHandler:
+    def __init__(self) -> None:
+        self.rotation = args.rotate
+        self.device = self._find_touchscreen()
+        self.name = str(self.device.name).replace(" ", "-").lower()
+        self.max_x, self.max_y = self._get_device_xy_limit()
+        self._capture_report()
+        self.toggle_input(False)
+
+    def events_loop(self):
+        return self.device.read_loop()
+
+    def _find_touchscreen(self):
+        context = pyudev.Context()
+        for device in context.list_devices(subsystem="input"):
+            if (
+                device.properties.get("ID_INPUT_TOUCHSCREEN") == "1"
+                and device.device_node is not None
+            ):
+                dev = InputDevice(device.device_node)
+                return dev
+        raise NoValidDeviceError("no touchscreen detected")
+
+    def _get_device_xy_limit(self):
+        max_x = max_y = 0
+        for abs_code, abs_info in self.device.capabilities().get(ecodes.EV_ABS, []):
+            if abs_code == ecodes.ABS_X:
+                max_x = abs_info.max
+            elif abs_code == ecodes.ABS_Y:
+                max_y = abs_info.max
+        return max_x, max_y
+
+    def _capture_report(self):
+        print(f"Capturing input from {self.name}")
+        print(f"Max X: {self.max_x}, Max Y: {self.max_y}")
+        print(f"Active rotation: {self.rotation}°")
+
+    def toggle_input(self, enabled):
+        if enabled:
+            subprocess.run(
+                ["hyprctl", "-r", "keyword", f"device[{self.name}]:enabled", "1"]
+            )
+            print(f"{self.name} enabled")
+        else:
+            subprocess.run(
+                ["hyprctl", "-r", "keyword", f"device[{self.name}]:enabled", "0"]
+            )
+            print(f"{self.name} disabled")
+
+    def apply_rotation(self, x, y):
+        if self.rotation == 0:
+            return x, y
+        elif self.rotation == 180:
+            return (self.max_x - x, self.max_y - y)
+        elif self.rotation == 90:
+            return (y, self.max_x - x)
+        elif self.rotation == 270:
+            return (self.max_y - y, x)
+        else:
+            raise ValueError("Invalid rotation value")
+
+
+args = parse_arguments()
+
+TAP_MOVEMENT_THRESHOLD = args.tap_threshold
+TAP_DURATION_THRESHOLD = args.tap_duration
+SCROLL_MOVEMENT_THRESHOLD = args.scroll_threshold
+
+
+track_pad = TrackPad()
+touchscreen_handler = TouchscreenHandler()
+
+# variable initialization
+touch_start_time = None
+last_x = last_y = None
+active_touches = {}
+current_slot = 0
+already_scrolled = False
+already_moved = False
+already_dragged = False
+
+
 try:
-    # load original input
-    touch_dev = find_touchscreen_udev()
-    track_pad = TrackPad()
-
-    # variable initialization
-    touch_dev_name = str(touch_dev.name).replace(" ", "-").lower()
-    max_x, max_y = get_device_xy_limit(touch_dev)
-    touch_start_time = None
-    last_x = last_y = None
-    active_touches = {}
-    current_slot = 0
-    already_scrolled = False
-    already_moved = False
-    already_dragged = False
-
-    capture_report(touch_dev_name, max_x, max_y, ORIENTATION)
-    toggle_original_input(touch_dev_name, False)
-
-    for event in touch_dev.read_loop():
+    for event in touchscreen_handler.events_loop():
         if event.type == ecodes.EV_ABS:
             if event.code == ecodes.ABS_MT_SLOT:
                 current_slot = event.value
@@ -212,10 +208,7 @@ try:
                         "initial_y": None,
                         "current_x": None,
                         "current_y": None,
-                        "lifted": False,
                     }
-                else:
-                    active_touches[current_slot]["lifted"] = True
 
             elif event.code == ecodes.ABS_MT_POSITION_X:
                 if current_slot in active_touches:
@@ -230,19 +223,19 @@ try:
                     active_touches[current_slot]["current_y"] = event.value
 
         elif event.type == ecodes.EV_SYN and event.code == ecodes.SYN_REPORT:
-            if len(active_touches) != 0:
-                avg_x = sum(t["current_x"] for t in active_touches.values()) / len(
-                    active_touches
+            valid_touches_set = [
+                t for t in active_touches.values() if t["current_x"] and t["current_y"]
+            ]
+            if len(valid_touches_set) != 0:
+                avg_x = sum(t["current_x"] for t in valid_touches_set) / len(
+                    valid_touches_set
                 )
-                avg_y = sum(t["current_y"] for t in active_touches.values()) / len(
-                    active_touches
+                avg_y = sum(t["current_y"] for t in valid_touches_set) / len(
+                    valid_touches_set
                 )
-                rx, ry = apply_rotation(
+                rx, ry = touchscreen_handler.apply_rotation(
                     avg_x,
                     avg_y,
-                    ORIENTATION,
-                    max_x,
-                    max_y,
                 )
                 if not already_moved:
                     if all(
@@ -305,5 +298,5 @@ try:
                 # do not need to clear last_xy explicitly here
                 # as my if logic, the first coords report after touch is ignored
 except KeyboardInterrupt:
-    toggle_original_input(touch_dev_name, True)
+    touchscreen_handler.toggle_input(True)
     print("Restored touchscreen input")
