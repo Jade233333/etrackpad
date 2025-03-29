7020e44222e156b4dbb2ff85e598f63e3076868b
Jade <hatesjade@gmail.com>
2025-03-29 20:51:06 +0800

    objective recognizer donw

--- etrackpad.py
+++ etrackpad.py
@@ -16,22 +16,16 @@ def parse_arguments():
         help="Screen rotation (0, 90, 180, 270 degrees)",
     )
     parser.add_argument(
-        "--tap-threshold",
+        "--movement-threshold",
         type=int,
         default=50,
-        help="Tap movement threshold",
+        help="movement threshold to distinguish between a tap and cursor movement",
     )
     parser.add_argument(
-        "--tap-duration",
+        "--duration_threshold",
         type=float,
         default=0.12,
-        help="Tap duration threshold",
-    )
-    parser.add_argument(
-        "--scroll-threshold",
-        type=int,
-        default=50,
-        help="Scroll movement threshold",
+        help="Time duration threshold to distinguish between a tap and cursor movement",
     )
     parser.add_argument(
         "--scroll-sensitivity",
@@ -50,7 +44,7 @@ def parse_arguments():
 
 
 class TrackPad:
-    def __init__(self):
+    def __init__(self, max_x, max_y):
         self.scroll_sensitivity = args.scroll_sensitivity
         self.cursor_sensitivity = args.cursor_sensitivity
         self.device = uinput.Device(
@@ -63,6 +57,20 @@ class TrackPad:
                 uinput.REL_HWHEEL,
             ]
         )
+        self.max_x, self.max_y = max_x, max_y
+        self.rotation = args.rotate
+
+    def _apply_rotation(self, x, y):
+        if self.rotation == 0:
+            return x, y
+        elif self.rotation == 180:
+            return (self.max_x - x, self.max_y - y)
+        elif self.rotation == 90:
+            return (y, self.max_x - x)
+        elif self.rotation == 270:
+            return (self.max_y - y, x)
+        else:
+            raise ValueError("Invalid rotation value")
 
     def _qualify(self, x):
         qualified_x = 0
@@ -73,12 +81,16 @@ class TrackPad:
         return qualified_x
 
     def move_cursor(self, current_x, current_y, last_x, last_y):
+        current_x, current_y = self._apply_rotation(current_x, current_y)
+        last_x, last_y = self._apply_rotation(last_x, last_y)
         rel_x = int((current_x - last_x) * self.cursor_sensitivity)
         rel_y = int((current_y - last_y) * self.cursor_sensitivity)
         self.device.emit(uinput.REL_X, rel_x)
         self.device.emit(uinput.REL_Y, rel_y)
 
     def scroll_wheel(self, current_x, current_y, last_x, last_y):
+        current_x, current_y = self._apply_rotation(current_x, current_y)
+        last_x, last_y = self._apply_rotation(last_x, last_y)
         rel_x = self._qualify(current_x - last_x) * self.scroll_sensitivity
         rel_y = self._qualify(current_y - last_y) * self.scroll_sensitivity
         self.device.emit(uinput.REL_HWHEEL, -rel_x)
@@ -166,48 +178,29 @@ class TouchscreenHandler:
                 ["hyprctl", "-r", "keyword", f"device[{self.name}]:enabled", "0"]
             )
 
-    def apply_rotation(self, x, y):
-        if self.rotation == 0:
-            return x, y
-        elif self.rotation == 180:
-            return (self.max_x - x, self.max_y - y)
-        elif self.rotation == 90:
-            return (y, self.max_x - x)
-        elif self.rotation == 270:
-            return (self.max_y - y, x)
-        else:
-            raise ValueError("Invalid rotation value")
 
+class GestureRecognizer:
+    def __init__(self):
+        self.movement_threshold = args.movement_threshold
+        self.duration_threshold = args.duration_threshold
+        self.touch_start_time = None
+        self.last_x = None
+        self.last_y = None
+        self.active_touches = {}
+        self.current_slot = 0
+        self.already_scrolled = False
+        self.already_moved = False
+        self.already_dragged = False
 
-args = parse_arguments()
-
-TAP_MOVEMENT_THRESHOLD = args.tap_threshold
-TAP_DURATION_THRESHOLD = args.tap_duration
-SCROLL_MOVEMENT_THRESHOLD = args.scroll_threshold
-
-
-track_pad = TrackPad()
-touchscreen_handler = TouchscreenHandler()
-
-# variable initialization
-touch_start_time = None
-last_x = last_y = None
-active_touches = {}
-current_slot = 0
-already_scrolled = False
-already_moved = False
-already_dragged = False
-
-
-try:
-    for event in touchscreen_handler.events_loop():
+    def process_event(self, event):
+        # Process ABS events for multitouch data
         if event.type == ecodes.EV_ABS:
             if event.code == ecodes.ABS_MT_SLOT:
-                current_slot = event.value
+                self.current_slot = event.value
 
             elif event.code == ecodes.ABS_MT_TRACKING_ID:
                 if event.value != -1:
-                    active_touches[current_slot] = {
+                    self.active_touches[self.current_slot] = {
                         "initial_x": None,
                         "initial_y": None,
                         "current_x": None,
@@ -215,92 +208,124 @@ try:
                     }
 
             elif event.code == ecodes.ABS_MT_POSITION_X:
-                if current_slot in active_touches:
-                    if active_touches[current_slot]["initial_x"] is None:
-                        active_touches[current_slot]["initial_x"] = event.value
-                    active_touches[current_slot]["current_x"] = event.value
+                if self.current_slot in self.active_touches:
+                    if self.active_touches[self.current_slot]["initial_x"] is None:
+                        self.active_touches[self.current_slot]["initial_x"] = (
+                            event.value
+                        )
+                    self.active_touches[self.current_slot]["current_x"] = event.value
 
             elif event.code == ecodes.ABS_MT_POSITION_Y:
-                if current_slot in active_touches:
-                    if active_touches[current_slot]["initial_y"] is None:
-                        active_touches[current_slot]["initial_y"] = event.value
-                    active_touches[current_slot]["current_y"] = event.value
+                if self.current_slot in self.active_touches:
+                    if self.active_touches[self.current_slot]["initial_y"] is None:
+                        self.active_touches[self.current_slot]["initial_y"] = (
+                            event.value
+                        )
+                    self.active_touches[self.current_slot]["current_y"] = event.value
 
+        # Process synchronization events to generate actions
         elif event.type == ecodes.EV_SYN and event.code == ecodes.SYN_REPORT:
-            valid_touches_set = [
-                t for t in active_touches.values() if t["current_x"] and t["current_y"]
+            valid_touches = [
+                t
+                for t in self.active_touches.values()
+                if t["current_x"] is not None and t["current_y"] is not None
             ]
-            if len(valid_touches_set) != 0:
-                avg_x = sum(t["current_x"] for t in valid_touches_set) / len(
-                    valid_touches_set
-                )
-                avg_y = sum(t["current_y"] for t in valid_touches_set) / len(
-                    valid_touches_set
-                )
-                rx, ry = touchscreen_handler.apply_rotation(
-                    avg_x,
-                    avg_y,
-                )
-                if not already_moved:
+            if valid_touches:
+                rx = sum(t["current_x"] for t in valid_touches) / len(valid_touches)
+                ry = sum(t["current_y"] for t in valid_touches) / len(valid_touches)
+                if not self.already_moved:
                     if all(
-                        abs(t["current_x"] - t["initial_x"]) > TAP_MOVEMENT_THRESHOLD
-                        or abs(t["current_y"] - t["initial_y"]) > TAP_MOVEMENT_THRESHOLD
-                        for t in active_touches.values()
+                        abs(t["current_x"] - t["initial_x"]) > self.movement_threshold
+                        or abs(t["current_y"] - t["initial_y"])
+                        > self.movement_threshold
+                        for t in self.active_touches.values()
                     ):
-                        already_moved = True
-                elif last_x is not None and last_y is not None:
-                    if len(active_touches) == 1:
-                        track_pad.move_cursor(
-                            current_x=rx,
-                            current_y=ry,
-                            last_x=last_x,
-                            last_y=last_y,
+                        self.already_moved = True
+                elif self.last_x is not None and self.last_y is not None:
+                    if len(self.active_touches) == 1:
+                        yield (
+                            "move_cursor",
+                            {
+                                "current_x": rx,
+                                "current_y": ry,
+                                "last_x": self.last_x,
+                                "last_y": self.last_y,
+                            },
                         )
-                    elif len(active_touches) == 2 and not already_scrolled:
-                        track_pad.scroll_wheel(
-                            current_x=rx,
-                            current_y=ry,
-                            last_x=last_x,
-                            last_y=last_y,
+                    elif len(self.active_touches) == 2 and not self.already_scrolled:
+                        yield (
+                            "scroll_wheel",
+                            {
+                                "current_x": rx,
+                                "current_y": ry,
+                                "last_x": self.last_x,
+                                "last_y": self.last_y,
+                            },
                         )
-                        already_scrolled = True
-                    elif len(active_touches) == 3:
-                        if not already_dragged:
-                            track_pad.click_button("left", "down")
-                            already_dragged = True
-                        track_pad.move_cursor(
-                            current_x=rx,
-                            current_y=ry,
-                            last_x=last_x,
-                            last_y=last_y,
+                        self.already_scrolled = True
+                    elif len(self.active_touches) == 3:
+                        if not self.already_dragged:
+                            yield ("click_button", {"button": "left", "action": "down"})
+                            self.already_dragged = True
+                        yield (
+                            "move_cursor",
+                            {
+                                "current_x": rx,
+                                "current_y": ry,
+                                "last_x": self.last_x,
+                                "last_y": self.last_y,
+                            },
                         )
-                last_x, last_y = rx, ry
+                self.last_x, self.last_y = rx, ry
 
+        # Process touch key events to generate click actions
         elif event.type == ecodes.EV_KEY and event.code == ecodes.BTN_TOUCH:
-            if event.value == 1:  # Touch down
-                touch_start_time = event.timestamp()
-            elif event.value == 0:  # Touch up
-                if touch_start_time is not None:
-                    touch_duration = event.timestamp() - touch_start_time
+            if event.value == 1:
+                self.touch_start_time = event.timestamp()
+            elif event.value == 0:
+                if self.touch_start_time is not None:
+                    touch_duration = event.timestamp() - self.touch_start_time
                     finger_tap = all(
-                        touch_duration < TAP_DURATION_THRESHOLD
+                        touch_duration < self.duration_threshold
                         and abs(t["current_x"] - t["initial_x"])
-                        < TAP_MOVEMENT_THRESHOLD
+                        < self.movement_threshold
                         and abs(t["current_y"] - t["initial_y"])
-                        < TAP_MOVEMENT_THRESHOLD
-                        for t in active_touches.values()
+                        < self.movement_threshold
+                        for t in self.active_touches.values()
                     )
                     if finger_tap:
-                        if len(active_touches) == 2:
-                            track_pad.click_button("right", "down")
-                        if len(active_touches) == 1:
-                            track_pad.click_button("left", "down")
-                track_pad.click_button("right", "up")
-                track_pad.click_button("left", "up")
-                already_dragged = already_scrolled = already_moved = False
-                active_touches.clear()
-                # do not need to clear last_xy explicitly here
-                # as my if logic, the first coords report after touch is ignored
+                        if len(self.active_touches) == 2:
+                            yield (
+                                "click_button",
+                                {"button": "right", "action": "down"},
+                            )
+                        elif len(self.active_touches) == 1:
+                            yield ("click_button", {"button": "left", "action": "down"})
+                # Release click for both buttons
+                yield ("click_button", {"button": "right", "action": "up"})
+                yield ("click_button", {"button": "left", "action": "up"})
+                # Reset states
+                self.already_dragged = False
+                self.already_scrolled = False
+                self.already_moved = False
+                self.active_touches.clear()
+
+
+args = parse_arguments()
+
+touchscreen_handler = TouchscreenHandler()
+track_pad = TrackPad(touchscreen_handler.max_x, touchscreen_handler.max_y)
+gesture_recognizer = GestureRecognizer()
+
+try:
+    for event in touchscreen_handler.events_loop():
+        for action, params in gesture_recognizer.process_event(event):
+            if action == "move_cursor":
+                track_pad.move_cursor(**params)
+            elif action == "scroll_wheel":
+                track_pad.scroll_wheel(**params)
+            elif action == "click_button":
+                track_pad.click_button(params["button"], params["action"])
 except KeyboardInterrupt:
     touchscreen_handler.toggle_input(True)
     print("Restored touchscreen input")
